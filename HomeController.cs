76ceb61f712de15bbf0bd0b3fd4103dc9a16d98b
Waqar Naeem <waqarnaeem2@hotmail.com>
2020-05-06 17:33:26 +0500

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace SimpleChatApplicationfrom_Waqar.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}

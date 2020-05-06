f30107cde10d8225625505bb51c46bcaacfc3211
Waqar Naeem <waqarnaeem2@hotmail.com>
2020-05-06 17:33:10 +0500

    Add files via upload

---
+++ ChatHub.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet;
+
+namespace SimpleChatApplicationfrom_Waqar.Models
+{
+    public class ChatHub : Hub
+    {
+        //public void Hello()
+        //{
+        //    Clients.All.hello(); // Hub will control chats --
+        //}
+
+        public void Send(string name, string message)
+        {
+            Clients.All.BroadcastMessage(name, message);
+        }
+    }
+}

bec742712ce509dae755681f6c5d4af863b96012
Waqar Naeem <waqarnaeem2@hotmail.com>
2020-05-06 17:32:45 +0500

    Add files via upload
    It should be at root Directory

---
+++ StartupOwin.cs
@@ -0,0 +1,34 @@
+﻿//using System;
+//using System.Threading.Tasks;
+//using Microsoft.Owin;
+//using Owin;
+
+//[assembly: OwinStartup(typeof(SimpleChatApplicationfrom_Waqar.StartupOwin))]
+
+//namespace SimpleChatApplicationfrom_Waqar
+//{
+//    public class StartupOwin
+//    {
+//        public void Configuration(IAppBuilder app)
+//        {
+//            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+//        }
+//    }
+//}
+
+using Microsoft.Owin;
+
+using Owin;
+
+[assembly: OwinStartup(typeof(SimpleChatApplicationfrom_Waqar.StartupOwin))]
+namespace SimpleChatApplicationfrom_Waqar
+{
+   public class StartupOwin
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+
+        }
+    }
+}
